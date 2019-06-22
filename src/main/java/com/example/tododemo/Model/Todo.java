8dcd5652e912bacadeb2f5ce40c639e0261feee9
Max Weis <maxweis@protonmail.com>
2019-06-22 18:54:51 +0200

    implement entity

---
+++ src/main/java/com/example/tododemo/Model/Todo.java
@@ -0,0 +1,70 @@
+package com.example.tododemo.Model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "todos")
+public class Todo{
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private int id;
+    private String title;
+    private String description;
+    private Date dueDate;
+    private boolean done;
+    
+    public Todo(String title, String description, Date dueDate, boolean done) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.done = done;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    
+}

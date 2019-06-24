7b3100056a4daff08a4f4e3cd9614e754f2ec0b7
Max Weis <maxweis@protonmail.com>
2019-06-24 20:32:07 +0200

    fix db

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,1 +1,13 @@
-INSERT INTO todos (title,description,dueDate,done) VALUES ("clean fridge","It's a mess","2018-08-27T12:34:56.789Z",false)
+CREATE TABLE todos(
+    id serial PRIMARY KEY,
+    title VARCHAR(30),
+    description VARCHAR(500),
+    duedate VARCHAR(254),
+    done BOOLEAN
+);
+
+INSERT INTO todos(title,description,duedate,done)VALUES ('clean fridge1','Its a mess','2018-08-27T12:34:56.789Z',false);
+INSERT INTO todos(title,description,duedate,done)VALUES ('clean fridge2','Its a mess','2018-08-27T12:34:56.789Z',false);
+INSERT INTO todos(title,description,duedate,done)VALUES ('clean fridge3','Its a mess','2018-08-27T12:34:56.789Z',false);
+INSERT INTO todos(title,description,duedate,done)VALUES ('clean fridge4','Its a mess','2018-08-27T12:34:56.789Z',false);
+INSERT INTO todos(title,description,duedate,done)VALUES ('clean fridge5','Its a mess','2018-08-27T12:34:56.789Z',false);
